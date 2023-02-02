26ddec857d3dab7e830fa30cd9c3615f5ab8a4ed
snezhanadosinova <sdosinova@gmail.com>
2023-02-02 14:29:01 +0200

    fix: added again the div with class App and set animate to x: -50%

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import { motion } from "framer-motion/dist/framer-motion";
 
 function App() {
   return (
-    <motion.form
-      animate={{ x: "0" }}
-      style={{
-        position: "absolute",
-        top: "15%",
-        transform: "translateX(-100vw)",
-      }}
-      transition={{ type: "spring", duration: 1 }}
-    >
-      <h1>Create An Account</h1>
-      <label>
-        Email:
-        <input name="email" type="email" required />
-      </label>
-      <label>
-        Password:
-        <input name="password" type="password" required />
-      </label>
-      <button>Sign Up!</button>
-    </motion.form>
+    <div className="App">
+      <motion.form
+        initial={{ x: "-100vw" }}
+        animate={{ x: "-50%" }}
+        style={{
+          position: "absolute",
+          top: "15%",
+        }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Create An Account</h1>
+        <label>
+          Email:
+          <input name="email" type="email" required />
+        </label>
+        <label>
+          Password:
+          <input name="password" type="password" required />
+        </label>
+        <button>Sign Up!</button>
+      </motion.form>
+    </div>
   );
 }
 
